2034382c4e2d883914c413fdd3113af3fcbd2888
Shazia <shaziaahmad528@gmail.com>
2026-05-28 22:03:43 +0400

    connected frontend to the backend

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,8 +1,36 @@
 import { motion } from "framer-motion"
 import Navbar from "../componenets/Navbar"
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 export default function Login () {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) :  Promise<void> => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:3000/login", {
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
+
+        const data: unknown = await response.json();
+
+        console.log(data);
+    }
+
+
+
+
     return(
         <div>
             <Navbar/>
@@ -14,23 +42,40 @@ export default function Login () {
                 transition={{ duration: 0.5, ease: "easeOut"}}
             >
                 
-                <div className="loginbox">
+                <form className="loginbox" onSubmit={handleSubmit}>
 
                     <div className="headerlogin">LOGIN</div>
 
                     <div className="authsubboxes">
+
                         <label htmlFor="Email"></label>
-                        <input type="text" placeholder="Email" id="Email" className="authinput"/>
+                        <input
+                            type="email" 
+                            value={email}
+                            placeholder="Email" 
+                            onChange={(e) => setEmail(e.target.value)}
+                            id="Email" 
+                            className="authinput"
+                        />
+
                     </div>
 
                     <div className="authsubboxes">
+
                         <label htmlFor="password"></label>
-                        <input type="password" placeholder="password" className="authinput"/>
+                        <input 
+                            type="password" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="password" 
+                            className="authinput"
+                        />
+
                     </div>
 
                     <button type="submit" className="authbtn">LOGIN</button>
                     <div className="authtext">Don't have an account?<Link to={"/signup"} > Sign Up </Link></div>
-                </div>
+                </form>
         
             </motion.div>
         </div>
--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  res.json({
+    message: "login received",
+    email,
+  });
+});
+
+
 app.get("/", (req, res) => {
   res.send("Server running");
 });
